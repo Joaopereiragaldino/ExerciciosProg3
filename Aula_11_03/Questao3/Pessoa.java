e808e994202c7e62331eae742d842e3f92306643
João Marcello Galdino Pereira <gjoaomarcello@gmail.com>
2025-12-07 10:40:40 -0300

    Construtores, Herança e Polimorfismo (Aula_11_03)

--- Aula_11_03/Questao3/Pessoa.java
+++ Aula_11_03/Questao3/Pessoa.java
@@ -4,77 +4,70 @@ public class Pessoa {
     private String nome;
     private int idade;
 
-    public Pessoa(){
+    public Pessoa() {
         this.nome = "Não inicializado";
         this.idade = -1;
     }
 
-    public Pessoa(String anome, int aidade){
+    public Pessoa(String anome, int aidade) {
         this.nome = anome;
-        if(aidade < 0){
+        if (aidade < 0) {
             this.idade = -1;
         } else {
             this.idade = aidade;
         }
     }
 
-
-    public String getNome(){
+    public String getNome() {
         return nome;
     }
 
-    public int getIdade(){
+    public int getIdade() {
         return idade;
     }
 
-    public void setNome(String anome){
+    public void setNome(String anome) {
         this.nome = anome;
     }
 
-    public void setIdade(int aidade){
+    public void setIdade(int aidade) {
         this.idade = aidade;
     }
 
-static public class Aluno extends Pessoa{
+    static public class Aluno extends Pessoa {
 
-    private String matricula;
+        private String matricula;
 
-    public String getMatricula(){
-        return matricula;
+        public Aluno(String anome, int aidade, String amatricula) {
+            super(anome, aidade);
+            this.matricula = amatricula;
+        }
+
+        public String getMatricula() {
+            return matricula;
+        }
+
+        public void setMatricula(String amatricula) {
+            this.matricula = amatricula;
+        }
+
+        public void ExibirDados() {
+            System.out.println("--- Dados do aluno ---");
+            System.out.println("Nome: " + this.getNome()); 
+            System.out.println("Idade: " + this.getIdade());
+            System.out.println("Matricula: " + this.getMatricula());
+        }
     }
 
-    @Override
-    public int getIdade() {
-        return super.getIdade();
-    }
+    public static void main(String[] args) {
+        Aluno a1 = new Aluno("Kleber", 5, "Matriculado");
+        
+        a1.ExibirDados(); 
 
-    @Override
-    public String getNome() {
-        return super.getNome();
+        System.out.println("\nAlterando nome e matrícula do Kleber...");
+        a1.setNome("Kleber Silva");
+        a1.setMatricula("Trancado");
+        
+        a1.ExibirDados();
     }
-
-    public Aluno(String anome, int aidade, String amatricula){
-        super(anome, aidade);
-        this.matricula = amatricula;
-    }
-
-    public void ExibirDados(Aluno a1){
-        System.out.println("Dados do aluno:");
-        System.out.print("Nome: ");
-        System.out.println(a1.getNome());
-        System.out.print("Idade: ");
-        System.out.println(a1.getIdade());
-        System.out.print("Matricula: ");
-        System.out.println(a1.getMatricula());
-        }    
 }
-public static void main(String[] args) {
-    Aluno a1 = new Aluno("Kleber", 5, "Matriculado");
-    Aluno a2 = new Aluno("Junior", 10, "Não Matriculado");
-
-    a1.ExibirDados(a1); 
-    a2.ExibirDados(a2);  
-}
-}
-
-
